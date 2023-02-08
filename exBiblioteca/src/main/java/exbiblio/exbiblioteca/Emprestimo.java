5c517e8cc8157de5adbff32502592054787d4999
User <User@DESKTOP-D57QA5C>
2023-02-08 18:00:33 -0300

    Emprestimo 1

---
+++ exBiblioteca/src/main/java/exbiblio/exbiblioteca/Emprestimo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exbiblio.exbiblioteca;
+
+/**
+ *
+ * @author User
+ */
+public class Emprestimo {
+    
+    private String dataEmprestimo;
+    private String horaEmprestimo;
+    
+   
+    
+    Livros livros;
+    Usuario usuario;
+
+
+public void fazerEmprestimo(){
+
+
+}
+
+
+}
